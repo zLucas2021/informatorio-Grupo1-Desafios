a688c3e67a089407ea601275696e6af888891c12
giulianaleoni <giuli_leoni@hotmail.com>
2023-06-13 14:27:54 -0300

    Create clases.py
    clase usuario

---
+++ desafio8grupo1/clases.py
@@ -0,0 +1,15 @@
+
+
+class Usuario:
+    def __init__(self, id, nombre, apellido, telefono, username, email, contraseña, fecha_registro, avatar, estado, online):
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.username = username
+        self.email = email
+        self.contraseña = contraseña
+        self.fecha_registro = fecha_registro
+        self.avatar = avatar
+        self.estado = estado
+        self.online = online
