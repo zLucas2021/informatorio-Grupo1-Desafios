5019994138990bcc5fceba3ea49c701a51239635
giulianaleoni <giuli_leoni@hotmail.com>
2023-06-15 16:19:52 -0300

    Update clases.py
    15/16 cambie los parametros como puso el profe hoy en la clase.
    
    y para el login volvimos a la lista a ver si ahi se puede

--- desafio8grupo1/clases.py
+++ desafio8grupo1/clases.py
@@ -1,8 +1,8 @@
 
 
 class Usuario:
-    usuarios_registrados = {}
-    def __init__(self, id, nombre, apellido, telefono, username, email, contraseña, fecha_registro, avatar, estado, online):
+    usuarios_registrados = []
+    def __init__(self, id, nombre, apellido, telefono, username, email, contraseña, fecha_registro, avatar):
         self.id = id
         self.nombre = nombre
         self.apellido = apellido
@@ -12,26 +12,23 @@ class Usuario:
         self.contraseña = contraseña
         self.fecha_registro = fecha_registro
         self.avatar = avatar
-        self.estado = estado
-        self.online = online
 
     def login(self, username, contraseña):
-        if username in self.usuarios_registrados:
-            usuario = self.usuarios_registrados[username]
-            if usuario.contraseña == contraseña:
-                self.id = usuario.id
-                self.nombre = usuario.nombre
-                self.apellido = usuario.apellido
-                self.telefono = usuario.telefono
-                self.email = usuario.email
-                self.fecha_registro = usuario.fecha_registro
-                self.avatar = usuario.avatar
-                self.estado = usuario.estado
-                self.online = usuario.online
-                print("Inicio de sesión exitoso.")
-                return
-        print("Credenciales incorrectas. Inténtalo nuevamente.")
-        return False
+        for usuario in self.usuarios_registrados:
+        if usuario.username == username and usuario.contraseña == contraseña:
+            self.id = usuario.id
+            self.nombre = usuario.nombre
+            self.apellido = usuario.apellido
+            self.telefono = usuario.telefono
+            self.email = usuario.email
+            self.fecha_registro = usuario.fecha_registro
+            self.avatar = usuario.avatar
+            self.estado = usuario.estado
+            self.online = usuario.online
+            print("Inicio de sesión exitoso.")
+            return True
+    print("Inténtalo nuevamente")
+    return False
 
     def registrar(self, id, nombre, apellido, telefono, username, email, contraseña, fecha_registro, avatar):
         nuevo_usuario = Usuario(id, nombre, apellido, telefono, username, email, contraseña, fecha_registro, avatar, "activo", True)
